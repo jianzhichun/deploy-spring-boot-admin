a60b3e715db720e11106e7f9052249ec7cd06fe3
jianzhichun <zzchun12826@gmail.com>
2017-04-09 09:23:31 +0800

    update

--- deploy-spring-boot-admin/src/main/webpack/components/action/actionCtrl.js
+++ deploy-spring-boot-admin/src/main/webpack/components/action/actionCtrl.js
@@ -2,19 +2,19 @@
 
 require('./action.css');
 module.exports =  {
-    bindings: {
-        action:'<action'
-    }, 
-    controller:function ($scope, $http) {
-        'ngInject';
-        var ctrl = this;
-        $scope.content = 'This is content';
-        $scope.action = ctrl.action;
-        $scope.takeAction = function() {
-            $http.get('api/deploy/doAction/' + ctrl.action).then(function (response) {
-                $scope.result = response.data;
-            }); 
-        }; 
-    }, 
-    template:require('./action.tpl.html')
+  bindings: {
+    action:'<action'
+  }, 
+  controller:function ($scope, $http) {
+    'ngInject';
+    var ctrl = this;
+    $scope.content = 'This is content';
+    $scope.action = ctrl.action;
+    $scope.takeAction = function() {
+      $http.get('api/deploy/doAction/' + ctrl.action).then(function (response) {
+        $scope.result = response.data;
+      }); 
+    }; 
+  }, 
+  template:require('./action.tpl.html')
 };
